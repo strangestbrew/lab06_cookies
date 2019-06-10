47963dc2d82d430af6597fe2b29da279d3e16ccd
strangestbrew <thomas.lyndsey@gmail.com>
2019-06-10 15:50:51 -0700

    pike object function began

--- js/app.js
+++ js/app.js
@@ -1,2 +1,36 @@
 "use strict";
+ 
+var times = ["6 am", "7 am", "8 am", "9 am", "10 am", "11 am", "12 pm", "1 pm", "2 pm", "3 pm", "4 pm", "5 pm", "6 pm", "7 pm", "8 pm"];
 
+ var pike = {
+     minCust = 23
+     maxCust = 65
+     avgCookie = 6.3
+
+     function generateRandom(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+      }
+
+      for (i = 0; i < times.length; i++);
+
+
+
+ }
+
+ var seaTac = {
+
+ }
+
+ var seattleCenter = {
+
+ }
+
+ var capHill = {
+
+ }
+
+ var Alki = {
+
+ }
