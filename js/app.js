f48e6c44a9a09aa6973e33d39049d63db51e041f
strangestbrew <thomas.lyndsey@gmail.com>
2019-06-10 17:48:25 -0700

    cookie inventory random generation complete for pike and seaTac

--- js/app.js
+++ js/app.js
@@ -2,35 +2,73 @@
  
 var times = ["6 am", "7 am", "8 am", "9 am", "10 am", "11 am", "12 pm", "1 pm", "2 pm", "3 pm", "4 pm", "5 pm", "6 pm", "7 pm", "8 pm"];
 
- var pike = {
-     minCust = 23
-     maxCust = 65
-     avgCookie = 6.3
+var pike = {
+    minCust: 23,
+    maxCust: 65,
+    avgCookie: 6.3,
 
-     function generateRandom(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+     generateRandom: function(min, max) {
+        return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
+    },
+    
+    cookiesPerHour: function () {
+        return Math.floor(this.generateRandom(this.minCust, this.maxCust)* this.avgCookie)
+    },    
+
+    render: function () {
+      for (var i=0; i < times.length; i++ ) {
+        console.log(`${times[i]}: ${this.cookiesPerHour()}`)
       }
+    }
+}
 
-      for (i = 0; i < times.length; i++);
+console.log(pike.generateRandom (pike.minCust, pike.maxCust), 'generateRandom');
 
+console.log(pike.cookiesPerHour(), 'cookiesPerHour');
 
+console.log(pike.render());
 
- }
 
  var seaTac = {
+     minCust = 3,
+     maxCust = 24,
+     avgCookie = 1.2,
+
+     generateRandom: function(min, max) {
+         return Math.floor(Math.random()*(this.maxCust - this.minCust + 1)) + this.minCust;
+     },
+
+     cookiesPerHour: function () {
+         return Math.floor(this.generateRandom(this.minCust, this.maxCust)* this.avgCookie)
+     },
+
+     render: function () {
+         for (var i=0; i <times.length; i++){
+             console.log(`${times[i]}: ${this.cookiesPerHour()}`)
+         }
+     }
 
  }
 
  var seattleCenter = {
+     //minCust = 11,
+     //maxCust = 38,
+     //avgCookie = 3.7
+
 
  }
 
  var capHill = {
+     //minCust = 20,
+     //maxCust = 38,
+     //avgCookie = 2.3,
 
  }
 
  var Alki = {
+     //minCust = 2,
+     //maxCust = 16,
+     //avgCookie = 4.6,
+
 
  }
